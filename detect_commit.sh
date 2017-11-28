8bca9303a9fa0be12b8412562d0ca71d3ca4619c
Stanislav Ivanov <stanislav.ivanov@abilixsoft.eu>
2017-11-28 07:43:16 +0000

    changed

--- detect_commit.sh
+++ detect_commit.sh
@@ -16,7 +16,11 @@ gitRev=`git rev-list  "$beforeC".."$afterC"`
 
 for hashes in $gitRev
  do
+<<<<<<< HEAD
    getResult=$getResult`git branch  --contains $hashes | sed s/\*//g | sed 's/ *//g' | grep "master"`$'\n'
+=======
+   getResult=$getResult`git branch  --contains $hashes | sed s/\*//g | sed 's/ *//g' | grep "$grepRegex"`$'\n'
+>>>>>>> ecb427d... changed
    sortResult=`echo $getResult | xargs -n1 | sort -fu`
 done
 

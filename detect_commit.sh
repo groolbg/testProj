8566a6a07cb32d6ee603b6ec6988288cfb121a54
Stanislav Ivanov <stanislav.ivanov@abilixsoft.eu>
2017-11-29 15:08:18 +0000

    fake date change

--- detect_commit.sh
+++ detect_commit.sh
@@ -15,16 +15,18 @@ if [[ $# -ne 1 ]]; then
     exit
 fi
 
-upToDate=0;
+ChangesDetected=0;
 for project in $BUILDPROJECTS
 do
-  git -C $REPOPATH/$cFolder/ pull 2>&1 | grep -e "^Already up-to-date\.$"
+  echo  git -C $REPOPATH/$cFolder/ pull | grep -e "^Already up-to-date\.$"
+   git -C $REPOPATH/$cFolder/ pull  | grep -e "^Already up-to-date\.$"
   if [[ $? -ne 0 ]]; then
-    upToDate=1 
+    ChangesDetected=1 
+    echo "Changes detected!"
   fi
 done
 
-if [[ $upToDate -eq 1 ]]; then
+if [[ $ChangesDetected -eq 1 ]]; then
    echo curl "http://ulautomat:2a633d89702d42e402ff6920bc313d34@ulautomation2017.abilixsoft.eu:8080/job/testGraphPrj/buildWithParameters?token=asd&vTag=$cFolder"
 fi 
 
